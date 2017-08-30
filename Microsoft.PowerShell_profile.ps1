fdf459d6fcb99b772e829923a1bde926a58aa359
carlbergh <tomi.o.carlbergh@gmail.com>
2017-08-30 07:28:35 +0200

    powershell profile file

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,50 @@
+set-location C:\
+function prompt
+{
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    #                                                                              Rikard Ronnkvist / snowland.se
+    # Multicolored prompt with marker for windows started as Admin and marker for providers outside filesystem
+    # Examples
+    #    C:\Windows\System32>
+    #    [Admin] C:\Windows\System32>
+    #    [Registry] HKLM:\SOFTWARE\Microsoft\Windows>
+    #    [Admin] [Registry] HKLM:\SOFTWARE\Microsoft\Windows>
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ 
+    # New nice WindowTitle
+    $Host.UI.RawUI.WindowTitle = "PowerShell v" + (get-host).Version.Major + "." + (get-host).Version.Minor + " (" + $pwd.Provider.Name + ") " + $pwd.Path
+ 
+    # Admin ?
+    if( (
+        New-Object Security.Principal.WindowsPrincipal (
+            [Security.Principal.WindowsIdentity]::GetCurrent())
+        ).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+    {
+        # Admin-mark in WindowTitle
+        $Host.UI.RawUI.WindowTitle = "[Admin] " + $Host.UI.RawUI.WindowTitle
+ 
+        # Admin-mark on prompt
+        Write-Host "[" -nonewline -foregroundcolor DarkGray
+        Write-Host "Admin" -nonewline -foregroundcolor Red
+        Write-Host "] " -nonewline -foregroundcolor DarkGray
+    }
+ 
+    # Show providername if you are outside FileSystem
+    if ($pwd.Provider.Name -ne "FileSystem") {
+        Write-Host "[" -nonewline -foregroundcolor DarkGray
+        Write-Host $pwd.Provider.Name -nonewline -foregroundcolor Gray
+        Write-Host "] " -nonewline -foregroundcolor DarkGray
+    }
+ 
+    # Split path and write \ in a gray
+    $pwd.Path.Split("\") | foreach {
+        Write-Host $_ -nonewline -foregroundcolor Yellow
+        Write-Host "\" -nonewline -foregroundcolor Gray
+    }
+ 
+    # Backspace last \ and write >
+    Write-Host "`b>" -nonewline -foregroundcolor Gray
+ 
+    return " "
+}
+Clear-Host
